f087e09f5dcc7b19873762e15966cfa07e1610ad
이정윤(Jung Yoon Lee) <jylee020529@gmail.com>
2024-05-15 01:06:32 +0900

    add onload

--- map.js
+++ map.js
@@ -3,16 +3,15 @@ require("dotenv").config();
 const script = document.createElement('script');
 script.defer = true;
 script.async = true;
-script.src = `https://oapi.map.naver.com/openapi/v3/maps.js?ncpClientId=${process.env.CLIENT_ID}&callback=initMap`;
-document.body.appendChild(script);
-
-function initMap() {
+script.src = `https://oapi.map.naver.com/openapi/v3/maps.js?ncpClientId=${process.env.CLIENT_ID}`;
+script.addEventListener("load", function() {
     var mapOptions = {
         center: new naver.maps.LatLng(37.3595704, 127.105399),
         zoom: 10
     };
     
     var map = new naver.maps.Map('map', mapOptions);
-}
+});
 
+document.body.appendChild(script);
 
